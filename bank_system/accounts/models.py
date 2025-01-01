f86e9cd8b8048d47214eb4cd02fb0947b0525d7a
RajChandan <raj.chandan91@gmail.com>
2025-01-01 23:58:45 +0530

    account model

--- bank_system/accounts/models.py
+++ bank_system/accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ("DEPOSIT", "Deposit"),
+        ("WITHDRAWAL", "Withdrawal"),
+    ]
+    type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} - ${self.amount}"
